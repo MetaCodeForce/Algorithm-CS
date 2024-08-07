a5f4408ac161ab296126003d95d13e3731f35ab0
jisuuu6 <jisuuu6@gmail.com>
2024-08-08 00:49:11 +0900

    이지수 문제풀이 수정

--- 20240807/Jisoo.java
+++ 20240807/Jisoo.java
@@ -25,4 +25,35 @@ class Solution {
 
         return sb.toString();
     }
+
+    public String convert2(String s, int numRows) {
+        HashMap<Integer, StringBuilder> map = new HashMap<>();
+
+        int idx = 0;
+        int plus = 1;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb = map.getOrDefault(idx, new StringBuilder());
+            sb.append(s.charAt(i));
+            map.put(idx, sb);
+
+            idx = idx + plus;
+
+            if (idx == -1 || idx == numRows) {
+                idx = idx - plus;
+            }
+
+            if (idx == 0 || idx == numRows - 1) {
+                plus = plus * (-1);
+            }
+        }
+
+        sb = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            sb.append(map.getOrDefault(i, new StringBuilder()));
+        }
+
+        return sb.toString();
+    }
 }
