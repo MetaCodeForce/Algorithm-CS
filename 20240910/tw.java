887135e8a21f6f4dd0b3910382ee2feb9d3f01b3
YTW <57580401+dbxodnsms@users.noreply.github.com>
2024-09-10 18:03:11 +0900

    tw.java 업데이트

---
+++ 20240910/tw.java
@@ -0,0 +1,12 @@
+Class Solution{
+	pubcli Boolean solution(String s, String p){
+		// Pattern 객체 생성
+		Pattern pattern = Pattern.compile(p);
+		
+		// Matcher 객체 생성
+		Matcher matcher = pattern.matcher(s);
+
+		// 결과
+		return matcher.matches();
+	}
+}
